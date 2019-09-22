9cfe1f370b801f47ce07a91a9b2b7952465fa763
ilya permyakov <ilya@callfire.com>
2019-09-22 14:45:35 +0200

    Lesson 4 Task 2

--- 4. Counting Elements/tasks/frog_river_one.php
+++ 4. Counting Elements/tasks/frog_river_one.php
@@ -1,6 +1,4 @@
 <?php
-
-
 function solution1($a, $x)
 {
     $n = count($a);
@@ -22,15 +20,51 @@ function solution1($a, $x)
         if ($done && $max == $x) {
             break;
         }
-
     }
 
     return ($done && $max == $x) ? $i : -1;
 }
 
-//6
-echo solution1([1, 3, 1, 4, 2, 3, 5, 4], 5);
-//-1
-echo solution1([1, 1, 1, 1], 2);
-// 4
-echo solution1([1, 3, 1, 3, 2, 1, 3], 3);
+////6
+//echo solution1([1, 3, 1, 4, 2, 3, 5, 4], 5);
+////-1
+//echo solution1([1, 1, 1, 1], 2);
+//// 4
+//echo solution1([1, 3, 1, 3, 2, 1, 3], 3);
+
+
+function solution2($a, $x)
+{
+    $n = count($a);
+    $cnt = [];
+    $max = null;
+    $sum1 = 0;
+    for ($i = 0; $i < $n; $i++) {
+        $done = true;
+        if (!isset($cnt[$a[$i]])) {
+            $cnt[$a[$i]] = 1;
+            $sum1 += $a[$i];
+            $max = max($max, $a[$i]);
+        }
+        if (is_null($max)) {
+            $max = $a[$i];
+        }
+
+        $sum2 = $max * ($max + 1) / 2;
+        if ($sum1 != $sum2) {
+            $done = false;
+        }
+
+        if ($done && $max == $x) {
+            break;
+        }
+    }
+
+    return ($done && $max == $x) ? $i : -1;
+}
+////6
+echo solution2([1, 3, 1, 4, 2, 3, 5, 4], 5);
+///-1
+echo solution2([1, 1, 1, 1], 2);
+///4
+echo solution2([1, 3, 1, 3, 2, 1, 3], 3);
