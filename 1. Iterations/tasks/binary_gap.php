4a8c97209c8bc8f0f071e11f8fb8eb3c031f114f
ilya permyakov <ilya@callfire.com>
2019-09-22 18:58:41 +0200

    Lesson 1 Task 1

--- 1. Iterations/tasks/binary_gap.php
+++ 1. Iterations/tasks/binary_gap.php
@@ -1,27 +1,26 @@
 <?php
+//straight approach
 function solution(int $n)
 {
     $s = base_convert($n, 10, 2);
     $n = strlen($s);
-    $gap = 0;
     $len = 0;
     $max = null;
+    $gap = true;
+
     for ($i = 0; $i < $n; $i++) {
         if ($s[$i] == '1') {
             if (!$gap) {
-                $gap = 1;
-            }
-            if ($gap == 2) {
                 if (is_null($max)) {
                     $max = $len;
                 }
                 $max = max($len, $max);
                 $len = 0;
-                $gap = 1;
+                $gap = true;
             }
         } else {
-            if ($gap == 1) {
-                $gap = 2;
+            if ($gap) {
+                $gap = false;
             }
             $len++;
         }
@@ -29,5 +28,6 @@ function solution(int $n)
     return is_null($max) ? 0 : $max;
 }
 
-
-echo solution(32);
+assert(solution(529) == 4);
+assert(solution(32) == 0);
+echo 'Success';
