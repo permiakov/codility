8a264ef947456e5b1d9def3478a1036f0ceb68e9
ilya permyakov <ilya@callfire.com>
2019-09-23 21:35:12 +0200

    Lesson 4 Task 3

--- 4. Counting Elements/tasks/max_counters.php
+++ 4. Counting Elements/tasks/max_counters.php
@@ -1,32 +1,23 @@
 <?php
 
-function solution1(int $n, array $a)
+function solution(int $n, array $a)
 {
     $m = count($a);
     $r = array_fill(0, $n, 0);
     $max = 0;
-    $visited = [];
     $maxZero = 0;
     for ($i = 0; $i < $m; $i++) {
         $index = $a[$i] - 1;
         if (($a[$i] <= $n)) {
-            if ($r[$index] == 0 && $maxZero > 0) {
-                $r[$index] = $maxZero;
-            }
+            $r[$index] = max($maxZero, $r[$index]);
             $r[$index]++;
             $max = max($max, $r[$index]);
-            $visited[] = $index;
         } else {
             $maxZero = $max;
-            foreach ($visited as $visIndex) {
-                $r[$visIndex] = $max;
-            }
         }
     }
     foreach ($r as $i => $value) {
-        if ($r[$i] == 0) {
-            $r[$i] += $maxZero;
-        }
+            $r[$i] = max($r[$i], $maxZero);
     }
 
     return $r;
