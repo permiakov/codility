705112a564c95c7b9341d9903fdce533b63bb295
ilya permyakov <ilya@callfire.com>
2019-09-25 14:11:15 +0200

    Lesson 5 Exercise 2

--- 5. Prefix sums/exersises/mushrooms_picker_prefix.php
+++ 5. Prefix sums/exersises/mushrooms_picker_prefix.php
@@ -1,24 +1,53 @@
 <?php
-//simple prefix sums
-//allows to calculate quickly the sum of needed slice of array
+
 function prefix_sums(array $a)
 {
     $n = count($a);
-    //array +1 size of original array
     $p = array_fill(0, $n + 1, 0);
-    //starting from 1
     for ($i = 1; $i <= $n; $i++) {
-        //p[3] = a[0] + a[1] + a[2]
         $p[$i] = $p[$i - 1] + $a[$i - 1];
     }
     return $p;
 }
 
-//total between x and y 
 function count_total($p, $x, $y)
 {
     return $p[$y + 1] - $p[$x];
 }
 
-//$p = prefix_sums([1, 2, 3]);
-//echo count_total($p, 0, 2);
+function mushrooms(array $a, int $k, int $m)
+{
+    $n = count($a) - 1;
+    $max = 0;
+    $sums = prefix_sums($a);
+    //start  moving to the left and then the right
+    for ($p = 0; $p < $m; $p++) {
+        $stepsLeft = $p;
+        $realStepsLeft = min($k, $stepsLeft);
+        $leftBorder = $k - $realStepsLeft;
+
+        $stepsRight = $m - $stepsLeft;
+        $realStepsRight = min($n - $leftBorder, $stepsRight);
+        $rightBorder = $leftBorder + $realStepsRight;
+
+        $max = max($max, count_total($sums, $leftBorder, $rightBorder));
+    }
+    //moving to the right and then the left
+    for ($p = 0; $p < $m; $p++) {
+        $stepsRight = $p;
+        $realStepsRight = min($p, $n - $k);
+        $rightBorder = $k + $realStepsRight;
+
+        $stepsLeft = $m - $stepsRight;
+        $realStepsLeft = min(($k + $realStepsRight), $stepsLeft);
+        $leftBorder = $rightBorder - $realStepsLeft;
+
+        $max = max($max, count_total($sums, $leftBorder, $rightBorder));
+    }
+    return $max;
+}
+
+assert(ASSERT_EXCEPTION, 1);
+assert(mushrooms([2, 3, 7, 5, 1, 3, 9], 4, 6) == 25);
+
+echo 'Success';
