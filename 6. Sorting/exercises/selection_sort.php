b22bb3d03bacaa03bf0f08aa0c659cf63ef11301
ilya permyakov <ilya@callfire.com>
2019-09-25 17:17:51 +0200

    Lesson 6 Exercise 1

---
+++ 6. Sorting/exercises/selection_sort.php
@@ -0,0 +1,23 @@
+<?php
+//The idea: Find the minimal element and swap it with the first element of an array.
+//Next, just sort the rest of the array, without the first element, in the same way.
+function solution(array $a)
+{
+    $n = count($a);
+    for ($i = 0; $i < $n; $i++) {
+        $min = $minIndex = 0;
+        for ($j = $i + 1; $j < $n; $j++) {
+            if ($a[$i] > $a[$j]) {
+                $min = $a[$j];
+                $minIndex = $j;
+            }
+        }
+        if ($min) {
+            $t = $a[$i];
+            $a[$i] = $min;
+            $a[$minIndex] = $t;
+        }
+    }
+}
+
+solution([2, 3, 1, 5]);
